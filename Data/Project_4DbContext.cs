2c49decfad323ab3818825a192ea1070de48a81b
kishan.kumar4@mphasis.com <kishan.kumar4@mphasis.com>
2023-09-07 13:10:20 +0530

    first commit

---
+++ Data/Project_4DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Project_4.Models;
+
+namespace Project_4.Data
+{
+    public class Project_4DbContext : DbContext
+    {
+        public Project_4DbContext (DbContextOptions<Project_4DbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Project_4.Models.RainbowSchool> RainbowSchool { get; set; } = default!;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Project_4.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Project_4DbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Project_4DbContext") ?? throw new InvalidOperationException("Connection string 'Project_4DbContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
